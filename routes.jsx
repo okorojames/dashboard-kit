a2fe6010f75dd37529e0b2d0217564b240a12226
Okoro James <okorojameschizaram@gmail.com>
2023-10-17 18:54:53 +0100

    setting up files and folder strctures

--- routes.jsx
+++ routes.jsx
@@ -5,12 +5,14 @@ import {
 } from "react-router-dom";
 import AppLayout from "./src/layouts/AppLayout";
 import Home from "./src/pages/home/Home";
+import Tickets from "./src/pages/tickets/Tickets";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<AppLayout />}>
       <Route index element={<Home />} />
-      <Route path="/overview" element={<Home />} />
+      <Route path="overview" element={<Home />} />
+      <Route path="tickets" element={<Tickets />} />
     </Route>
   )
 );
